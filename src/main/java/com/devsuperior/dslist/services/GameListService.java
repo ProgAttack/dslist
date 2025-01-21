5b3d51fac15beb208bee4dd63fe13fd9367770b1
ProgAttack <gianramiress@hotmail.com>
2025-01-21 15:15:42 -0300

    GET lists

--- src/main/java/com/devsuperior/dslist/services/GameListService.java
+++ src/main/java/com/devsuperior/dslist/services/GameListService.java
@@ -10,29 +10,23 @@ import com.devsuperior.dslist.dto.GameDTO;
 import com.devsuperior.dslist.dto.GameListDTO;
 import com.devsuperior.dslist.dto.GameMinDTO;
 import com.devsuperior.dslist.entities.Game;
-import com.devsuperior.dslist.repositories.GameRepository;
+import com.devsuperior.dslist.entities.GameList;
+import com.devsuperior.dslist.repositories.GameListRepository;
 
 @Service
-public class GameService {
+public class GameListService {
 	
 	@Autowired
-	private GameRepository gameRepository;
+	private GameListRepository gameListRepository;
 	
 	@Transactional(readOnly = true)
-	public List<GameMinDTO> findAll(){		
-		List<Game> result = gameRepository.findAll();
-		List<GameMinDTO> dto = result.stream().map(x -> new GameMinDTO(x)).toList();
+	public List<GameListDTO> findAll(){		
+		List<GameList> result = gameListRepository.findAll();
+		List<GameListDTO> dto = result.stream().map(x -> new GameListDTO(x)).toList();
 		return dto;
 		
 	}
 	
-	@Transactional(readOnly = true)
-	public GameDTO findById(Long id) {
-		Game result = gameRepository.findById(id).get();
-		GameDTO dto = new GameDTO(result);
-		return dto;
-		
-	}
-	
+
 
 }
