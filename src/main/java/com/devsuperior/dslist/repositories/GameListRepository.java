83e5c1bf0884adfe2b139cf4712a4bf8b475a886
ProgAttack <gianramiress@hotmail.com>
2025-01-23 11:25:07 -0300

    Move games on list

--- src/main/java/com/devsuperior/dslist/repositories/GameListRepository.java
+++ src/main/java/com/devsuperior/dslist/repositories/GameListRepository.java
@@ -1,11 +1,20 @@
 package com.devsuperior.dslist.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 
 import com.devsuperior.dslist.entities.GameList;
 
-public interface GameListRepository extends JpaRepository<GameList, Long>{
+import jakarta.transaction.Transactional;
 
+public interface GameListRepository extends JpaRepository<GameList, Long>{
+	
+	@Modifying
+	@Transactional
+	@Query(nativeQuery = true, 
+	value = "UPDATE tb_belonging SET position = :newPosition WHERE list_id = :listId AND game_id = :gameId")
+	void updateBelongingPosition(Long listId, Long gameId, Integer newPosition);
 	
 	
 }
